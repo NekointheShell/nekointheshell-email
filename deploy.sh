99c002d8f9a0d69ad1a6e0db6add378e89d0fdc0
neko <neko@127.0.0.1>
2024-01-31 23:12:14 -0700

    change sasl passwd and restart services

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,10 @@ cp -R dovecot /etc/dovecot
 
 postmap /etc/postfix/sasl_password
 echo 'nameserver 8.8.8.8' > /var/spool/postfix/etc/resolv.conf
+
+read -s -p 'SASL Password: ' sasl
+sed -i "s/CHANGEME_SENDGRID/$sasl/" /etc/postfix/sasl_password
+
+
+systemctl restart postfix
+systemctl restart dovecot
